ae541c607796cb03b5495d87d880c2746d6ca252
Marcus <marcus.gamboa@gmail.com>
2021-02-03 17:32:07 -0700

    Added Animation

--- src/App.js
+++ src/App.js
@@ -2,31 +2,41 @@ import './App.css';
 import React, { Component } from 'react';
 import quotesArray from './quotesArray.js'
 
+let hexArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quoteNumber: 0
+      quoteNumber: 0,
+      color: ''
     }
     this.newQuote = this.newQuote.bind(this);
   }
   newQuote = () => {
-    console.log(quotesArray.length)
-
     let newQuoteNumber = Math.floor(Math.random() * (quotesArray.length - 1));
+    let newColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+      let hex = hexArray[Math.floor(Math.random() * hexArray.length)]
+      newColor += hex;
+    }
+    console.log(newColor)
     this.setState({
-      quoteNumber: newQuoteNumber
+      quoteNumber: newQuoteNumber,
+      color: newColor
     })
   }
   render() {
+
     return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: this.state.color}}>
       <div id="quote-box">
-        <h1 id="removeText">{quotesArray[this.state.quoteNumber].quote}</h1>
-        <p id="author">- {quotesArray[this.state.quoteNumber].author}</p>
+        <h1 id="text" style={{color: this.state.color}}>{quotesArray[this.state.quoteNumber].quote}</h1>
+        <p id="author" style={{color: this.state.color}}>- {quotesArray[this.state.quoteNumber].author}</p>
         <div className="buttons-container">
-          <a href="http://twitter.com/intent/tweet" target="_blank" id="tweet-quote"><button>Tweet Quote</button></a>
-          <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+          <a href="http://twitter.com/intent/tweet" target="_blank" rel="noreferrer" id="tweet-quote"><button style={{backgroundColor: this.state.color}}>Tweet Quote</button></a>
+          <button id="new-quote" onClick={this.newQuote} style={{backgroundColor: this.state.color}}>New Quote</button>
         </div>
       </div>
     </div>
--- src/quotesArray.js
+++ src/quotesArray.js
@@ -2,9 +2,14 @@ const QuotesArray = [
     {quote: "Do or do not, there is no try.", author: 'Yoda'},
     {quote: "Git 'R Done!", author: "Larry the Cable Guy"},
     {quote: "No.", author: "My Wife"},
-    {quote: "Do you Hear the People Sing?", author: "Jean Valjean"},
     {quote: "Live Long and Prosper 🖖", author: "Spock"},
-    {quote: "", author: ""}
+    {quote: "I have a dream...", author: "Martin Luther King Jr."},
+    {quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.", author: "Benjamin Franklin"},
+    {quote: "In the end, it's not the years in your life that count. It's the life in your years.", author: "Abraham Lincoln"},
+    {quote: "Do not let making a living prevent you from making a life.", author: "John Wooden"},
+    {quote: "The way to get started is to quit talking and begin doing.", author: "Walt Disney"},
+    {quote: '"Hello World!"', author: "Anyone who's every coded"},
+    {quote: "", author: ""},
 ]
 
 export default QuotesArray;
