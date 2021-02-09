db648101eec8764e1ef6b2891bea119fa8b11773
Marcus <marcus.gamboa@gmail.com>
2021-02-08 18:18:38 -0700

    I will get this someday

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import quotesArray from './quotesArray.js'
+import { connect } from 'react-redux';
 
 let hexArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
@@ -46,4 +47,7 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  color: state.color,
+})
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+
+import quotesArray from './quotesArray.js';
 
 /* REDUX STUFF */
 const COLOR = 'COLOR'
+const hexArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
-const colorAction = () => ({
-  type: COLOR
-})
+
+const newQuote = () => {
+  let quoteNumber = Math.floor(Math.random() * (quotesArray.length - 1));
+  let newColor = '#';
+
+  for (let i = 0; i < 6; i++) {
+    let hex = hexArray[Math.floor(Math.random() * hexArray.length)]
+    newColor += hex;
+  }
+    
+  return {
+    type: COLOR,
+    // quoteNumber,
+    newColor
+  }
+}
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -24,9 +40,51 @@ const reducer = (state, action) => {
 
 const store = createStore(reducer);
 
-store.dispatch(colorAction())
+store.dispatch(newQuote())
 
 /* END REDUX */
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quoteNumber: 0,
+      color: ''
+    }
+    this.newQuote = this.newQuote.bind(this);
+  }
+  newQuote = () => {
+    let newQuoteNumber = Math.floor(Math.random() * (quotesArray.length - 1));
+    let newColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+      let hex = hexArray[Math.floor(Math.random() * hexArray.length)]
+      newColor += hex;
+    }
+    console.log(newColor)
+    this.setState({
+      quoteNumber: newQuoteNumber,
+      color: newColor
+    })
+  }
+  componentDidMount() {
+    setTimeout(this.newQuote,1)
+  }
+  render() {
+    return (
+    <div className="App" style={{backgroundColor: this.state.color}}>
+      <div id="quote-box">
+        <h1 id="text" style={{color: this.state.color}}>{quotesArray[this.state.quoteNumber].quote}</h1>
+        <p id="author" style={{color: this.state.color}}>- {quotesArray[this.state.quoteNumber].author}</p>
+        <div className="buttons-container">
+          <a href="http://twitter.com/intent/tweet" target="_blank" rel="noreferrer" id="tweet-quote"><button style={{backgroundColor: this.state.color}}>Tweet Quote</button></a>
+          <button id="new-quote" onClick={this.newQuote} style={{backgroundColor: this.state.color}}>sNew Quote</button>
+        </div>
+      </div>
+    </div>
+  );
+  }
+}
 
 ReactDOM.render(
   <Provider store={store}>
@@ -34,6 +92,14 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+const mapStateToProps = (state) => ({
+  color: state.color,
+})
+connect()(App);
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
