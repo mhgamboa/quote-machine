985067227a44df7f509a80a7e744a3230c92c273
Marcus <marcus.gamboa@gmail.com>
2021-02-11 19:57:17 -0700

    Wow, I think I finally got it!!! :D

--- src/index.js
+++ src/index.js
@@ -1,47 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore, bindActionCreators } from 'redux';
+import { Provider, connect } from 'react-redux';
 
 import './App.css';
 
 import quotesArray from './quotesArray.js';
 
-/* START REDUX */
-const NEWCOLOR = 'NEWCOLOR';
+//Functions for Redux
+let createNewQuoteNumber = () => Math.floor(Math.random() * (quotesArray.length - 1));
+let createNewColor = () => {
+  let hexArray = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+  let newColor = '#';
+  
+  for (let i = 0; i < 6; i++) {
+    let hex = hexArray[Math.floor(Math.random() * hexArray.length)]
+    newColor += hex;
+  }
+  return newColor;
+}
 
-const reducer = (state = 'yellow', action) => {
+/* START REDUX */
+const NEWQUOTE = 'NEWQUOTE';
+
+let initialState = {
+  color: 'orange',
+  quoteNumber: 1
+}
+
+const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case NEWCOLOR:
-      return action.color;
+    case NEWQUOTE:
+      console.log(action)
+      return {
+  color: action.color,
+  quoteNumber: action.quoteNumber
+};
     default:
         return state;
   }
 }
-let newColorAction = () => ({
-  type: NEWCOLOR,
-  color: 'orange'
+let NEWQUOTEAction = () => ({
+  type: NEWQUOTE,
+  color: createNewColor(),
+  quoteNumber: createNewQuoteNumber()
 })
 
 const store = createStore(reducer);
-
-store.dispatch(newColorAction())
-
-console.log(store.getState())
-
 /* END REDUX */
 
+/* START REACT */
 class App extends React.Component {
   render() {
     return (
-    <div className="App" style={{backgroundColor: 'blue'}}>
+    <div className="App" style={{backgroundColor: this.props.color}}>
       <div id="quote-box">
-        <h1 id="text" style={{color: 'green'}}>{quotesArray[0].quote}</h1>
-        <p id="author" style={{color: 'green'}}>- {quotesArray[0].author}</p>
+        <h1 id="text" style={{color: this.props.color}}>{quotesArray[this.props.quoteNumber].quote}</h1>
+        <p id="author" style={{color: this.props.color}}>- {quotesArray[this.props.quoteNumber].author}</p>
         <div className="buttons-container">
-          <a href="http://twitter.com/intent/tweet" target="_blank" rel="noreferrer" id="tweet-quote"><button style={{backgroundColor: 'green'}}>Tweet Quote</button></a>
-          <button id="new-quote" style={{backgroundColor: 'green'}}>New Quote</button>
+          <a href="http://twitter.com/intent/tweet" target="_blank" rel="noreferrer" id="tweet-quote"><button style={{backgroundColor: this.props.color}}>Tweet Quote</button></a>
+          <button id="new-quote" style={{backgroundColor: this.props.color}}  onClick={() => this.props.createNewQuote()}>New Quote</button>
         </div>
       </div>
     </div>
@@ -49,7 +68,21 @@ class App extends React.Component {
   }
 }
 
+/* END REACT */
+
+/* CONNECT REDUX TO REACT */
+let mapDispatchToProps = (dispatch) => ({
+  createNewQuote: () => dispatch(NEWQUOTEAction())
+})
+
+let mapStateToProps = (state) => ({
+  color: state.color,
+  quoteNumber: state.quoteNumber
+})
+
+let CApp = connect(mapStateToProps, mapDispatchToProps)(App)
+
 ReactDOM.render(
-  <Provider><App /></Provider>,
+  <Provider store={store}><CApp /></Provider>,
   document.getElementById('root')
 );
